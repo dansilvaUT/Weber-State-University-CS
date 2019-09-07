296c22560b9d1d122f5784a7fe16efe1ea31117b
dan <dan13silv@gmail.com>
2019-09-06 21:16:06 -0600

    Added fraction class for Lab 2.

---
+++ CS 3230/Lab 2/src/Fraction.java
@@ -0,0 +1,23 @@
+public class Fraction {
+
+    private int numerator;
+    private int denominator;
+
+    /**
+     * @param numerator   int
+     * @param denominator int
+     */
+    public Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public Fraction(int numerator) {
+        denominator = 1;
+    }
+
+    public Fraction add(Fraction right){
+        return null;
+    }
+
+}
