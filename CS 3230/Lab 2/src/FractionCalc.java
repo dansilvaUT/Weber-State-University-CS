0262a40f2dc71d213275457635a63a819a274908
dan <dan13silv@gmail.com>
2019-09-10 22:06:12 -0600

    Added main method.
    Need to follow up on TODO.

---
+++ CS 3230/Lab 2/src/FractionCalc.java
@@ -0,0 +1,9 @@
+public class FractionCalc {
+    public static void main(String[] args) {
+        Fraction fraction = new Fraction(1, 6);
+        Fraction fraction2 = new Fraction(7, 8);
+        //TODO implement test methods
+        System.out.println(fraction.add(fraction2));
+        System.out.println(fraction.toString());
+    }
+}
