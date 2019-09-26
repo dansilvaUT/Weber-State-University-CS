cfa70d4d4f92c9d95cf2856251b1f27547e20bb4
dan <dan13silv@gmail.com>
2019-09-25 21:21:53 -0600

    Parent class for Game. Updated matches method for cleaner and concise code.

--- CS 3230/Lab 3/src/Tile.java
+++ CS 3230/Lab 3/src/Tile.java
@@ -3,14 +3,10 @@ public class Tile {
     /**
      * Checks if both objects are instances of the same class
      *
-     * @param inother other tile
+     * @param other other tile
      * @return true/false
      */
-    public boolean matches(Tile inother) {
-        if (this.getClass() == inother.getClass()) {
-            return true;
-        } else {
-            return false;
-        }
+    public boolean matches(Tile other) {
+        return this.getClass() == other.getClass();
     }
 }
