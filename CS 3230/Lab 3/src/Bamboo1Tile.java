f337bc4eb6d53d2b69f3dfb82934cc41c19a2a04
dan <dan13silv@gmail.com>
2019-09-27 18:48:59 -0600

    Bamboo1Tile that inherits abstract class Picture Tile.
    Returns a hardcoded "Bamboo1".

---
+++ CS 3230/Lab 3/src/Bamboo1Tile.java
@@ -0,0 +1,20 @@
+public class Bamboo1Tile extends PictureTile {
+    /**
+     * Constructor that initializes name of tile picture
+     *
+     * @param name
+     */
+    public Bamboo1Tile(String name) {
+        super("Sparrow");
+    }
+
+    /**
+     * toString method that returns a hard coded "Bamboo 1"
+     *
+     * @return
+     */
+    @Override
+    public String toString() {
+        return "Bamboo1";
+    }
+}
