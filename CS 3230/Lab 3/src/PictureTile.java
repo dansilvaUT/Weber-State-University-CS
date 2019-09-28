eb2433bfed75864e1c3ba9f9fb02817bcaedac4e
dan <dan13silv@gmail.com>
2019-09-27 18:50:33 -0600

    Picture Tile abstract class that inherits from the Tile class.

--- CS 3230/Lab 3/src/PictureTile.java
+++ CS 3230/Lab 3/src/PictureTile.java
@@ -1,2 +1,24 @@
-public abstract class PictureTile {
+public abstract class PictureTile extends Tile {
+
+    private String name;
+
+    /**
+     * Constructor that initializes name of tile picture
+     *
+     * @param name
+     */
+    public PictureTile(String name) {
+        this.name = name;
+    }
+
+    /**
+     * toString will return the name, which will be one of the following:
+     * "Chrysanthemum", "Orchid", "Plum", "Bamboo", "Spring", "Summer", "Fall", or "Winter".
+     *
+     * @return name of Tile
+     */
+    @Override
+    public String toString() {
+        return String.format("%s", name);
+    }
 }
